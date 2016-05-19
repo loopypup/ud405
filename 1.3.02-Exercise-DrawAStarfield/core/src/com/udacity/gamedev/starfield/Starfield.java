8dbc96d5036b5ebc13d1f57ac63220b8593b0e71
loopypup <loopypup@gmail.com>
2016-05-18 23:48:23 -0400

    add starfield+milkyway (note was sloppy code just to do the challenge faster)

--- 1.3.02-Exercise-DrawAStarfield/core/src/com/udacity/gamedev/starfield/Starfield.java
+++ 1.3.02-Exercise-DrawAStarfield/core/src/com/udacity/gamedev/starfield/Starfield.java
@@ -3,9 +3,12 @@ package com.udacity.gamedev.starfield;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g3d.particles.influencers.ColorInfluencer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
+
+import java.util.Random;
 
 /**
  * TODO: Start here!
@@ -36,22 +39,52 @@ public class Starfield extends ApplicationAdapter {
     private static final float STAR_DENSITY = 0.01f;
     ShapeRenderer shapeRenderer;
     Array<Vector2> stars;
+    Array<Vector2> milky;
+
 
     @Override
     public void create() {
         // TODO: Initialize a shapeRenderer
-
+        shapeRenderer = new ShapeRenderer();
         // TODO: Call initStars
-
+        initStars(STAR_DENSITY);
+        milkyWay(STAR_DENSITY);
     }
 
     public void initStars(float density) {
         // TODO: Figure out how many stars to draw. You'll need the screen dimensions, which you can get using Gdx.graphics.getWidth() and Gdx.graphics.getHeight().
-
+        int SCREENWIDTH = Gdx.graphics.getWidth();
+        int SCREENHEIGHT = Gdx.graphics.getHeight();
+        int starCount = (int)(SCREENHEIGHT * SCREENWIDTH * density);
         // TODO: Create a new array of Vector2's to hold the star positions
-
+        stars = new Array<Vector2>(starCount);
         // TODO: Use java.util.Random to fill the array of star positions
+        Random random = new Random();
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < starCount; i++)
+        {
+            x = random.nextInt(SCREENWIDTH);
+            y = random.nextInt(SCREENHEIGHT);
+            stars.add(new Vector2(x,y));
+        }
+    }
 
+    public void milkyWay(float density)
+    {
+        int SCREENWIDTH = Gdx.graphics.getWidth();
+        int SCREENHEIGHT = Gdx.graphics.getHeight();
+        int starCount = (int)(SCREENHEIGHT * SCREENWIDTH * (density*.5));
+        milky = new Array<Vector2>(starCount);
+
+        Random random = new Random();
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < starCount; i++) {
+            x = random.nextInt(SCREENWIDTH);
+            y = (int) (random.nextInt((int) (SCREENHEIGHT * .5)) + (SCREENHEIGHT * .25));
+            milky.add(new Vector2(x, y));
+        }
     }
 
     @Override
@@ -63,21 +96,35 @@ public class Starfield extends ApplicationAdapter {
     @Override
     public void render() {
         // TODO: Make the night sky black
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         // TODO: Begin a shapeRenderer batch using ShapeType.Point
-
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Point);
         // TODO: Loop through the star positions and use shapeRenderer to draw points
+        for(Vector2 star : stars)
+        {
+            shapeRenderer.point(star.x,star.y,0);
+            Random random = new Random();
+            float r = random.nextFloat();
+            float g = random.nextFloat();
+            float b = random.nextFloat();
+            shapeRenderer.setColor(r,g,b,1);
 
+        }
+        for(Vector2 milk : milky)
+        {
+            shapeRenderer.point(milk.x,milk.y,0);
+            shapeRenderer.setColor(1,1,1,1);
+        }
         // TODO: End the shapeRenderer batch
-
+        shapeRenderer.end();
     }
 
     @Override
     public void dispose() {
         // TODO: Dispose of our ShapeRenderer
-
+        shapeRenderer.end();
         super.dispose();
     }
 }
