149279311144b2477d142da2131b3e2b38646e05
loopypup <loopypup@gmail.com>
2016-05-20 02:34:02 -0400

    Render text font

--- 1.4.10-Exercise-WordCloud/core/src/com/udacity/gamedev/wordcloud/WordCloud.java
+++ 1.4.10-Exercise-WordCloud/core/src/com/udacity/gamedev/wordcloud/WordCloud.java
@@ -69,16 +69,17 @@ public class WordCloud extends ApplicationAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
 
-        for (Word word : words) {
+        for (Word word : words)
+        {
 
             // TODO: Set the font's scale using font.getData().setScale()
-
+            font.getData().setScale(word.scale);
 
             // TODO: Set the font's tint using font.setColor()
-
+            font.setColor(word.color);
 
             // TODO: Actually draw the word using font.draw()
-
+            font.draw(batch,word.letters, word.x * Gdx.graphics.getWidth(), word.y * Gdx.graphics.getHeight());
         }
         batch.end();
     }
