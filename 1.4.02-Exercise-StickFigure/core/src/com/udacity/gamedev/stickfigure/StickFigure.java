7988edcc821c30f86296720a8614b669a01db33a
loopypup <loopypup@gmail.com>
2016-05-19 23:48:02 -0400

    Add Stick Figure named Dude Stick

--- 1.4.02-Exercise-StickFigure/core/src/com/udacity/gamedev/stickfigure/StickFigure.java
+++ 1.4.02-Exercise-StickFigure/core/src/com/udacity/gamedev/stickfigure/StickFigure.java
@@ -3,6 +3,8 @@ package com.udacity.gamedev.stickfigure;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * TODO: Start here
@@ -20,20 +22,49 @@ import com.badlogic.gdx.graphics.GL20;
  */
 public class StickFigure extends ApplicationAdapter {
 
+    ShapeRenderer renderer;
+    int SCREENWIDTH;
+    int SCREENHEIGHT;
     @Override
-    public void create() {
-
+    public void create()
+    {
+        renderer = new ShapeRenderer();
     }
 
     @Override
-    public void dispose() {
-
+    public void dispose()
+    {
+        renderer.dispose();
     }
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        //i should of put this up earlier so this doesn't calculate everytime but w/e
+        SCREENWIDTH = Gdx.graphics.getWidth();
+        SCREENHEIGHT = Gdx.graphics.getHeight();
+        //body
+        renderer.begin(ShapeRenderer.ShapeType.Line);
 
+        renderer.line(new Vector2((int)(SCREENWIDTH*.5),(int)(SCREENHEIGHT*.5))
+                ,new Vector2((int)(SCREENWIDTH*.5),(int)(SCREENHEIGHT*.2)));
+        //arms
+        renderer.line(new Vector2((int)(SCREENWIDTH*.5),(int)(SCREENHEIGHT*.4))
+                ,new Vector2((int)(SCREENWIDTH*.6),(int)(SCREENHEIGHT*.3)));
+        renderer.line(new Vector2((int)(SCREENWIDTH*.5),(int)(SCREENHEIGHT*.4))
+                ,new Vector2((int)(SCREENWIDTH*.4),(int)(SCREENHEIGHT*.3)));
+        //legs
+        renderer.line(new Vector2((int)(SCREENWIDTH*.5),(int)(SCREENHEIGHT*.2))
+                ,new Vector2((int)(SCREENWIDTH*.55),(int)(SCREENHEIGHT*.05)));
+        renderer.line(new Vector2((int)(SCREENWIDTH*.5),(int)(SCREENHEIGHT*.2))
+                ,new Vector2((int)(SCREENWIDTH*.45),(int)(SCREENHEIGHT*.05)));
+        renderer.end();
+
+        //head last to go over the body
+        renderer.begin(ShapeRenderer.ShapeType.Filled);
+        renderer.circle((int)(SCREENWIDTH*.5),(int)(SCREENHEIGHT*.5),30);
+
+        renderer.end();
     }
 }
