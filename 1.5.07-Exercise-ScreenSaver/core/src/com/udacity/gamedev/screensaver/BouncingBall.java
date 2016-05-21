a3fb4ddccece0853761873687dd7fcf200aeaad4
loopypup <loopypup@gmail.com>
2016-05-21 02:28:35 -0400

    Add a lot of bouncey circles!

--- 1.5.07-Exercise-ScreenSaver/core/src/com/udacity/gamedev/screensaver/BouncingBall.java
+++ 1.5.07-Exercise-ScreenSaver/core/src/com/udacity/gamedev/screensaver/BouncingBall.java
@@ -22,7 +22,7 @@ import java.util.Random;
 public class BouncingBall {
 
     private static final Color COLOR = Color.RED;
-    private static final float RADIUS_FACTOR = 1.0f / 20;
+    private static final float RADIUS_FACTOR = 1.0f / 300;
     private static final float KICK_VELOCITY = 500.0f;
 
     float radius;
@@ -50,7 +50,8 @@ public class BouncingBall {
     public void update(float delta, Viewport viewport) {
 
         // TODO: Update the ball's position using its velocity
-
+        position.x += delta * velocity.x;
+        position.y += delta * velocity.y;
 
         collideWithWalls(radius, viewport.getWorldWidth(), viewport.getWorldHeight());
     }
@@ -66,9 +67,19 @@ public class BouncingBall {
         }
 
         // TODO: Make the ball bounce off the bottom of the screen
+        if(position.y - radius <= 0)
+        {
+            position.y = radius;
+            velocity.y*=-1;
+        }
 
 
         // TODO: Make the ball bounce off the top of the screen
+        if(position.y + radius >= viewportHeight)
+        {
+            position.y = viewportHeight - radius;
+            velocity.y *= -1;
+        }
 
     }
 
